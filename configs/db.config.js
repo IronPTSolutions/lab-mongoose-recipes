4d0f0e9305a1764e6f1eff5ab63fdb3f46d697c1
Carlos del Prado <carlos.delprado@11paths.com>
2018-06-22 22:59:40 +0200

    feat: create, update, delete and close db

--- configs/db.config.js
+++ configs/db.config.js
@@ -5,6 +5,7 @@ const MONGO_URI = `mongodb://localhost/${DB_NAME}`;
 mongoose.connect(MONGO_URI);
 mongoose.connection.on('connected', (error) => {
     console.log('Connected to the database:', MONGO_URI);
+    mongoose.connection.db.dropDatabase();
 });
 mongoose.connection.on('error', (error) => {
     console.log('Database connection error:', error);
--- recipes.js
+++ recipes.js
@@ -1,10 +1,54 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const Recipe = require('./models/recipe.model');
+const mockRecipes = require('./data.js')
 
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+require('./configs/db.config');
+
+// Iteration 2
+const recipe = {
+  title: 'Beef and Pepper Stir Fry',
+  level: 'Amateur Chef',
+  ingredients: [
+    '8 oz beef strips',
+    '2 teaspoons corn flour',
+    '1 small onion',
+    '1/2 red pepper',
+    '1/2 green pepper',
+    '2 tablespoons vegetable oil',
+    '1 teaspoons minced garlic',
+    '1 teaspoons minced ginger',
+    '1/2 cup water'
+  ],
+  cuisine: 'Fusion',
+  dishType: 'Dish',
+  image: 'http://www.iaba.com/cookbook/images/recipe_19.png',
+  duration: 37,
+  creator: 'Valentina Kulma'
+}
+
+Recipe.create(recipe)
+  .then((recipe) => {
+    console.info(recipe.title);
+    
+    return Recipe.insertMany(mockRecipes)
+  })
+  .then((recipes) => {
+    for (let recipe of recipes) {
+      console.info(recipe.title);
+    }
+    
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { $set: { duration: 100 } }, { new: true });
+  })
+  .then((recipe) => {
+    console.log(`${recipe.title} successfully updated!`);
+
+    return Recipe.findOneAndRemove({ title: 'Carrot Cake' });
+  })
+  .then((recipe) => {
+    console.log(`${recipe.title} successfully removed!`);
+
+    return mongoose.connection.close();
+  })
+  .catch(error => {
+    console.error(error);
   });
